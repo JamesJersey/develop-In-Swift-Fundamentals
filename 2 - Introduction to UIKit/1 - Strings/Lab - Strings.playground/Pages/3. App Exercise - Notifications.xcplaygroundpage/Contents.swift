b1960e509bf9498c14983b2d913a85861aa9e020
JamesJersey <jamesjersey@outlook.com>
2025-03-30 19:54:30 -0400

    structures Completed

---
+++ 2 - Introduction to UIKit/1 - Strings/Lab - Strings.playground/Pages/3. App Exercise - Notifications.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+ 
+
+/*:
+ Occasionally users of your fitness tracking app will beat previous goals or records. You may want to notify them when this happens for encouragement purposes. Create a new constant `congratulations` and assign it a string literal that uses string interpolation to create the following string:
+ 
+- "Congratulations, <INSERT USER'S FULL NAME HERE>! You beat your previous daily high score of <INSERT PREVIOUS HIGHEST STEPS HERE> steps by walking <INSERT NEW HIGHEST STEPS HERE> steps yesterday!"
+ 
+ Insert `fullName`, `previousBest` and `newBest` where indicated. Print the value of `congratulations`.
+ */
+let previousBest = 14392
+let newBest = 15125
+let fullName = "Alexander"
+let congratulations = "Congrats, \(fullName)! You beat your previous daily high score of \(previousBest) steps by walking \(newBest) steps yesterday!"
+/*:
+[Previous](@previous)  |  page 3 of 5  |  [Next: Exercise - String Equality and Comparison](@next)
+ */
